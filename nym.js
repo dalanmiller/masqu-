aedfd7e908f1f9c1bd3e0ef1c7833cf8989b90df
Daniel Alan Miller <dalan.miller@gmail.com>
2014-10-17 00:36:52 -0400

    First commit

---
+++ nym.js
@@ -0,0 +1,18 @@
+
+console.log("BACKGROUND RUNNING");
+
+
+console.log("HELLO HELLO HELLO");
+
+var a = "Hello"
+window.a = "HELLLO"
+
+$(function(){
+    $('textarea').addClass('private');
+});
+
+$('window').click(function(){
+    $('textarea').addClass('private');
+})
+
+//Theater designed by <a href="http://www.thenounproject.com/bjorna1">Björn Andersson</a> from the <a href="http://www.thenounproject.com">Noun Project</a>
